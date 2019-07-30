c9e447f5834406b1ca7bf905276bf09cb3f280c2
JongHeonLee <ismir1449@gmail.com>
2019-07-30 14:12:54 +0900

    Add

--- study2/src/study2/Test107.java
+++ study2/src/study2/Test107.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 
 public class Test107 {
     public static void main(String[] args) {
-        String l = "이것은 글을 쓴..";
+        String l = "이것은 글을 쓴 내용에 해당하는 #해쉬태그 문장입니다..";
         /*
             문장에서 공백을 기준으로 하나씩 잘라서 볼때 사용한다.
             String 의 split 함수도 비슷한 기능을 하지만 이쪽이 더 가볍다.
@@ -12,7 +12,7 @@ public class Test107 {
         StringTokenizer st =new StringTokenizer( l );
         while (st.hasMoreTokens()){
             String tkn = st.nextToken();
-            System.out.println(tkn.indexOf("해당") != -1);
+            System.out.println(tkn.startsWith("해당") ); //금칙어는 startswith가 더 적함
             if(tkn.charAt(0) == '#'){
                 System.out.println(tkn);
             }
